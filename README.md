7eeb4c34537b38b1f9292d8696ece1d47c2eca28
Mihai BOSIICA <bosiic_m@etna-alternance.net>
2021-04-27 21:04:35 +0200

    Hangman with extra steps

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Hangman
+README:
+
+NOTA BENE:
+
+    The project's server was NOT made in GOLANG, but in NODE-EXPRESS.
+    I just couldn't get around learning GOLANG, working from home and having a headache every single hour,
+    so i took the decision to try improve my JS skills 
+    ( as i'll be needing that for the 6 month project which i'll be doing in JS ).
+    I'm prepared to assume the consequences of my choice.
+    
+Project name:
+        
+        "guesswhat" A.K.A. "Hangman with extra steps"
+
+Team members: 
+
+        Ionut-Mihai BOSIICA ---- ionut-mihai.bosiica@epitech.eu
+
+Technologies used:
+
+        Front: HTML/CSS/JQUERY
+        
+        Back: NODE-EXPRESS/SOCKET.IO
+        
+RUN STEPS:
+
+        npm install
+        
+        npm start
+        
+        open 2 browser pages on localhost:8080, create 2 players ( or more ) and test it out.
+        
+KNOWN BUGS:
+        
+        no responsive
+        
+        drawing for plebs looses quality ( it's not like the original )
+        
---
+++ server.js
@@ -0,0 +1,108 @@
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+var wordList = ["family", "cat", "dog", "Batman",
+    "woman", "bike", "flower", "croissant",
+    "fish", "smile", "feather", "cake"];
+
+/**
+ * @return {string}
+ */
+// returns random word for game
+function RandomWord() {
+    let wordIndex = Math.floor(Math.random() * (wordList.length));
+    return wordList[wordIndex];
+}
+
+var playerList = [];
+
+io.on('connection', function (socket) {
+    io.emit('playerList', playerList);
+
+    // player joins game
+    socket.on('join', function (player) {
+        socket.username = player;
+        socket.join(player);
+        playerList.push(player);
+
+        // if player is first user or if king room has no user then create new king
+        if (playerList.length === 1 || typeof io.sockets.adapter.rooms['king'] === 'undefined') {
+            socket.join('king');
+            io.in(player).emit('king', player);
+            io.in(player).emit('work your magic', RandomWord());
+
+            // else if king exists, everyone is pleb
+        } else {
+            socket.join('plebs');
+            io.in(player).emit('plebs', player);
+        }
+        // update playerList
+        io.emit('playerList', playerList);
+    });
+
+    // player leaves game
+    socket.on('disconnect', function () {
+        for (var i = 0; i < playerList.length; i++) {
+            if (playerList[i] === socket.username) {
+                playerList.splice(i, 1);
+            }
+        }
+        io.emit('playerList', playerList);
+
+        // if disconnected player is king, reassign new king
+        if (typeof io.sockets.adapter.rooms['king'] === "undefined") {
+            var newKing = Math.floor(Math.random() * (playerList.length));
+            io.in(playerList[newKing]).emit('newKing', playerList[newKing]);
+            io.in(playerList[newKing]).emit('newKing', playerList[newKing]);
+        }
+    });
+
+    // reassign new king
+    socket.on('newKing', function (player) {
+        socket.leave('plebs');
+        socket.join('king');
+        socket.emit('king', player);
+        io.in('king').emit('work your magic', RandomWord());
+    });
+
+    // king can swap role to pleb
+    socket.on('swapRole', function (data) {
+        socket.leave('king');
+        socket.join('plebs');
+        socket.emit('plebs', socket.username);
+        io.in(data.to).emit('king', data.to);
+        io.in(data.to).emit('work your magic', RandomWord());
+        io.emit('reset', data.to);
+    });
+
+    // draw
+    socket.on('draw', function (element) {
+        socket.broadcast.emit('draw', element);
+    });
+
+    // player's guess
+    socket.on('playerTurn', function (data) {
+        io.emit('playerTurn', {username: data.username, playerTurn: data.playerTurn});
+    });
+
+    // clear screen
+    socket.on('clearScreen', function (player) {
+        io.emit('clearScreen', player);
+    });
+
+    // correct answer
+    socket.on('correctAnswer', function (data) {
+        io.emit('correctAnswer', data);
+    });
+});
+
+server.listen(process.env.PORT || 8080, function () {
+    console.log('Please navigate to http://localhost:8080');
+});
